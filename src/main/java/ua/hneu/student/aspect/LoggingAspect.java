ac3af02c9e8398a26c397a14c7de0bf6ab0ec323
margaritazainullina <margarita.zainullina@gmail.com>
2014-03-11 00:10:37 +0200

    log4g added

--- src/main/java/ua/hneu/student/aspect/LoggingAspect.java
+++ src/main/java/ua/hneu/student/aspect/LoggingAspect.java
@@ -8,20 +8,24 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 
+import org.apache.log4j.*;
+
 @Aspect
 public class LoggingAspect {
+
+    static final Logger logger = Logger.getLogger(LoggingAspect.class);
 
     //launching setters
     @After("execution(* ua.hneu.student.domain.Group.setGroupId(..))")
     public void logAroundGroupId(JoinPoint joinPoint) {
-        System.out.println("Method : " + joinPoint.getSignature().getName() + "(" + Arrays.toString(joinPoint.getArgs()) + ")" + " is launched");
-        System.out.println("Inserting value GroupId =\"" + joinPoint.getArgs() + "\" to database");
+        logger.info("Method : " + joinPoint.getSignature().getName() + "(" + Arrays.toString(joinPoint.getArgs()) + ")" + " is launched");
+        logger.info("Inserting value GroupId =\"" + joinPoint.getArgs() + "\" to database");
     }
 
     @After("execution(* ua.hneu.student.domain.Speciality.setSpecialityId(..))")
     public void logAroundSpecialityId(JoinPoint joinPoint) {
-        System.out.println("Method : " + joinPoint.getSignature().getName() + "(" + Arrays.toString(joinPoint.getArgs()) + ")" + " is launched");
-        System.out.println("Inserting value SpecialityId =\"" + joinPoint.getArgs() + "\" to database");
+        logger.error("Method : " + joinPoint.getSignature().getName() + "(" + Arrays.toString(joinPoint.getArgs()) + ")" + " is launched");
+        logger.error("Inserting value SpecialityId =\"" + joinPoint.getArgs() + "\" to database");
     }
 
     //Launched when inserting null value in foreign key field
@@ -29,16 +33,16 @@ public class LoggingAspect {
             pointcut = "execution(* ua.hneu.student.domain.Group.setGroupId(..))",
             throwing = "error")
     public void logAfterThrowingGroupId(JoinPoint joinPoint, Throwable error) {
-        System.out.println("Method : " + joinPoint.getSignature().getName() + "(" + Arrays.toString(joinPoint.getArgs()) + ")" + " is launched");
-        System.out.println("Exception : " + error);
+        logger.error("Method : " + joinPoint.getSignature().getName() + "(" + Arrays.toString(joinPoint.getArgs()) + ")" + " is launched");
+        logger.error("Exception : " + error);
     }
 
     @AfterThrowing(
             pointcut = "execution(* ua.hneu.student.domain.Speciality.setSpecialityId(..))",
             throwing = "error")
     public void loglogAfterThrowingSpecialityId(JoinPoint joinPoint, Throwable error) {
-        System.out.println("Method : " + joinPoint.getSignature().getName() + "(" + Arrays.toString(joinPoint.getArgs()) + ")" + " is launched");
-        System.out.println("Exception : " + error);
+        logger.error("Method : " + joinPoint.getSignature().getName() + "(" + Arrays.toString(joinPoint.getArgs()) + ")" + " is launched");
+        logger.error("Exception : " + error);
     }
 
     //After launching getters
@@ -46,16 +50,16 @@ public class LoggingAspect {
             pointcut = "execution(* ua.hneu.student.domain.Speciality.getGroups(..))",
             returning = "result")
     public void logAfterReturningGroups(JoinPoint joinPoint, Object result) {
-        System.out.println("Method : " + joinPoint.getSignature().getName() + "()" + " is launched");
-        System.out.println("Method getGroups returned value is : " + result);
+        logger.error("Method : " + joinPoint.getSignature().getName() + "()" + " is launched");
+        logger.error("Method getGroups returned value is : " + result);
     }
 
     @AfterReturning(
             pointcut = "execution(* ua.hneu.student.domain.Group.getStudent(..))",
             returning = "result")
     public void logAfterReturningStudents(JoinPoint joinPoint, Object result) {
-        System.out.println("Method : " + joinPoint.getSignature().getName() + "()" + " is launched");
-        System.out.println("Method getGroups returned value is : " + result);
+        logger.error("Method : " + joinPoint.getSignature().getName() + "()" + " is launched");
+        logger.error("Method getGroups returned value is : " + result);
     }
 
 }
